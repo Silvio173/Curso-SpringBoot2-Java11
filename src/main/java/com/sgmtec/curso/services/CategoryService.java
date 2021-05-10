d663ec1dd101a972a238c44d07afb0d1eb5b7f42
Silvio Silva <silvio@geslab>
2021-05-10 19:14:52 -0300

    Category

---
+++ src/main/java/com/sgmtec/curso/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.sgmtec.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sgmtec.curso.entities.Category;
+import com.sgmtec.curso.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
