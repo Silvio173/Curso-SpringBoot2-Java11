0f8300d52e4a9a6695f452321c3aa6e15c8ae110
Silvio Silva <silvio@geslab>
2021-05-12 02:41:43 -0300

    Payment, one-to-one association

--- src/main/java/com/sgmtec/curso/config/TestConfig.java
+++ src/main/java/com/sgmtec/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.sgmtec.curso.entities.Category;
 import com.sgmtec.curso.entities.Order;
 import com.sgmtec.curso.entities.OrderItem;
+import com.sgmtec.curso.entities.Payment;
 import com.sgmtec.curso.entities.Product;
 import com.sgmtec.curso.entities.User;
 import com.sgmtec.curso.entities.enums.OrderStatus;
@@ -83,6 +84,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay = new Payment(null,Instant.parse("2019-06-20T21:53:07Z") , o1);
+		o1.setPayment(pay);
+		orderRepository.save(o1);
 		
 		
 		
